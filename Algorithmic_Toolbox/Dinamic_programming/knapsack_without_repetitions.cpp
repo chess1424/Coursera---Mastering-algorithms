869a3acf832a9abef8a5fb27382ca8bbb13408aa
Carlos Roberto Martinez Ojeda <carlos.1424@live.com.mx>
2016-10-25 13:09:20 -0500

    Knapsack without repetitions

--- Algorithmic_Toolbox/Dinamic_programming/knapsack_without_repetitions.cpp
+++ Algorithmic_Toolbox/Dinamic_programming/knapsack_without_repetitions.cpp
@@ -5,9 +5,9 @@
   for i from 1 to n:
     for w from 1 to w:
       value(w,i) <- value(w, i-1)
-	    if(wi <= w)
+      if(wi <= w)
         val <- value(w - wi, i - 1) + vi
-	      if value(w, i) < val
+        if value(w, i) < val
           value(w , i) <- val      `
   return value(W,n)
 */
